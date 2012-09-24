b32d70061b6f3c0b9b1ba60b99ae62a7d7426de3
Azadeh Sadeghieh <azadeh_sad@yahoo.com>
2012-09-25 09:17:53 +1000

    added indesx page to display hello message

---
+++ src/Squawkings/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Squawkings.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Hello Squawkings";
+            return View();
+        }
+
+    }
+}
